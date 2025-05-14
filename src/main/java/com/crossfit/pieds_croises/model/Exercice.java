755291710fa2212205103953e88be7228230549b
matthieu <matthieu@wegrow-app.com>
2025-05-14 15:26:39 +0200

    Add exercice model and entity Remove existing getters and setters and replace with lombok annotation on user model

---
+++ src/main/java/com/crossfit/pieds_croises/model/Exercice.java
@@ -0,0 +1,32 @@
+package com.crossfit.pieds_croises.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Exercice {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "measure_type")
+    private MeasureType measureType;
+
+    public enum MeasureType{
+        REPETITION,
+        WEIGHT,
+    }
+
+}
