70d47bd2604c185982874e83d915344fd668f5af
Marie-Sail <marie.saillard.pro@gmail.com>
2025-05-14 17:48:18 +0200

    fix: MessageRepository

--- src/main/java/com/crossfit/pieds_croises/repository/MessageRepository.java
+++ src/main/java/com/crossfit/pieds_croises/repository/MessageRepository.java
@@ -1,7 +1,7 @@
 package com.crossfit.pieds_croises.repository;
 
-import com.crossfit.pieds_croises.model.Exercice;
+import com.crossfit.pieds_croises.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageRepository extends JpaRepository<Exercice, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
