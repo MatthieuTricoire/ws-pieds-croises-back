90a953d87abefbffda860aa55649e2114af65e54
Marie-Sail <marie.saillard.pro@gmail.com>
2025-06-11 10:30:52 +0200

    feat: implement data.sql with messages

--- src/main/java/com/crossfit/pieds_croises/repository/MessageRepository.java
+++ src/main/java/com/crossfit/pieds_croises/repository/MessageRepository.java
@@ -2,6 +2,11 @@ package com.crossfit.pieds_croises.repository;
 
 import com.crossfit.pieds_croises.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
