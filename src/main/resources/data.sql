94d993ffdc33c9fdb5ac817a4d5dac754f0484f0
Agattepaille <amelie.gattepaille@gmail.com>
2025-06-11 15:56:53 +0200

    feature : initialisation BDD avec jeu de données

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,27 +1,3 @@
--- Supprimer les données
-DELETE
-FROM exercice;
-DELETE
-FROM subscription;
-DELETE
-FROM message;
-DELETE
-FROM box;
-DELETE
-FROM user;
-
-
--- Remettre à zéro les auto-incréments
-ALTER TABLE subscription
-    AUTO_INCREMENT = 1;
-ALTER TABLE exercice
-    AUTO_INCREMENT = 1;
-ALTER TABLE box
-    AUTO_INCREMENT = 1;
-ALTER TABLE user
-    AUTO_INCREMENT = 1;
-
-
 -- Peupler la table Box
 INSERT INTO box (name, address, city, zipcode, schedule, created_at, updated_at)
 VALUES ('Pieds croisés Paris', '12 Champs-Élysée', 'Paris', '72000', 'Lun-Ven 08h-18h', NOW(), NOW());
@@ -30,14 +6,18 @@ INSERT INTO box (name, address, city, zipcode,
 VALUES ('Pieds croisés Pau', '1 rue des Pyrénées', 'Pau', '64000', 'Lun-Ven 08h-18h', NOW(), NOW());
 
 -- Peupler la table Message
-INSERT INTO message (title, content, message_type, start_date, expiration_date, box_id, created_at,updated_at)
-VALUES ('Oyez Oyez info', 'Venez nombreux à notre super évent', 'INFORMATION', '2025-05-17', '2025-06-01', 1, '2025-05-01T10:00:00', '2025-05-01T10:00:00' );
 INSERT INTO message (title, content, message_type, start_date, expiration_date, box_id, created_at, updated_at)
-VALUES ('Oyez Oyez alert', 'Venez nombreux à notre super évent', 'ALERT', '2025-08-17', '2025-12-01', 1, '2025-05-01T10:00:00', '2025-05-01T10:00:00' );
+VALUES ('Oyez Oyez info', 'Venez nombreux à notre super évent', 'INFORMATION', '2025-05-17', '2025-06-01', 1,
+        '2025-05-01T10:00:00', '2025-05-01T10:00:00');
 INSERT INTO message (title, content, message_type, start_date, expiration_date, box_id, created_at, updated_at)
-VALUES ('Oyez Oyez event', 'Venez nombreux à notre super évent', 'EVENEMENT', '2025-05-17', '2025-12-01', 1, '2025-05-01T10:00:00', '2025-05-01T10:00:00' );
+VALUES ('Oyez Oyez alert', 'Venez nombreux à notre super évent', 'ALERT', '2025-08-17', '2025-12-01', 1,
+        '2025-05-01T10:00:00', '2025-05-01T10:00:00');
 INSERT INTO message (title, content, message_type, start_date, expiration_date, box_id, created_at, updated_at)
-VALUES ('Oyez Oyez rappel', 'Venez nombreux à notre super évent', 'RAPPEL', '2025-05-17', '2025-12-01', 1, '2025-05-01T10:00:00', '2025-05-01T10:00:00' );
+VALUES ('Oyez Oyez event', 'Venez nombreux à notre super évent', 'EVENEMENT', '2025-05-17', '2025-12-01', 1,
+        '2025-05-01T10:00:00', '2025-05-01T10:00:00');
+INSERT INTO message (title, content, message_type, start_date, expiration_date, box_id, created_at, updated_at)
+VALUES ('Oyez Oyez rappel', 'Venez nombreux à notre super évent', 'RAPPEL', '2025-05-17', '2025-12-01', 1,
+        '2025-05-01T10:00:00', '2025-05-01T10:00:00');
 
 
 -- Peupler la table Subscription
