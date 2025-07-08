2447bf83c9884a3118b9f996db7d030f06df2b65
Agattepaille <amelie.gattepaille@gmail.com>
2025-07-08 16:31:33 +0200

    feature : register user with first login

--- src/main/java/com/crossfit/pieds_croises/service/UserService.java
+++ src/main/java/com/crossfit/pieds_croises/service/UserService.java
@@ -1,21 +1,27 @@
 package com.crossfit.pieds_croises.service;
 
+import com.crossfit.pieds_croises.dto.FirstLoginDto;
 import com.crossfit.pieds_croises.dto.UserDto;
 import com.crossfit.pieds_croises.exception.ResourceNotFoundException;
 import com.crossfit.pieds_croises.mapper.UserMapper;
 import com.crossfit.pieds_croises.model.User;
 import com.crossfit.pieds_croises.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @AllArgsConstructor
 @Service
 public class UserService {
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
     public List<UserDto> getAllUsers() {
         List<User> users = userRepository.findAll();
@@ -38,11 +44,21 @@ public class UserService {
             User user = userMapper.convertToEntity(userDto);
             user.setCreatedAt(LocalDateTime.now());
             user.setUpdatedAt(LocalDateTime.now());
+
+            // génération du token pour la première connexion
+            String token = UUID.randomUUID().toString();
+            user.setRegistrationToken(token);
+            user.setTokenExpiryDate(LocalDateTime.now().plusDays(2));
+            user.setFirstLoginComplete(false);
+            // Envoi du lien par email
+            String registrationUrl = "https://votre-domaine.com/first-login?token=" + token;
+            emailService.sendInvitationEmail(user.getEmail(), registrationUrl);
+
             User createdUser = userRepository.save(user);
+
             return userMapper.convertToCreatedDto(createdUser);
         } catch (Exception e) {
-            System.err.println("Error creating user: " + e.getMessage());
-            throw e;
+            throw new RuntimeException("Error creating user", e);
         }
     }
 
@@ -70,5 +86,40 @@ public class UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
         userRepository.delete(user);
         return true;
+    }
+
+    public UserDto inviteUserByEmail(String email) {
+        User user = new User();
+        user.setEmail(email);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+        user.setFirstLoginComplete(false);
+        user.setRoles(Set.of("ROLE_USER"));
+
+        String token = UUID.randomUUID().toString();
+        user.setRegistrationToken(token);
+        user.setTokenExpiryDate(LocalDateTime.now().plusDays(2));
+
+        userRepository.save(user);
+
+        // TO DO : implémenter envoi d'email
+
+        return userMapper.convertToDtoForAdmin(user);
+    }
+
+    public void completeFirstLogin(FirstLoginDto dto) {
+        User user = userRepository.findByRegistrationToken(dto.getToken())
+                .orElseThrow(() -> new ResourceNotFoundException("Lien invalide"));
+
+        if (user.getTokenExpiryDate().isBefore(LocalDateTime.now())) {
+            throw new RuntimeException("Lien expiré");
+        }
+
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setRegistrationToken(null); // Token à usage unique
+        user.setFirstLoginComplete(true);
+        user.setUpdatedAt(LocalDateTime.now());
+
+        userRepository.save(user);
     }
 }
