5dfae1743e9a32dc6cc8a4f8e3ac35e401ecf806
Agattepaille <amelie.gattepaille@gmail.com>
2025-07-08 16:38:58 +0200

    feature : replace d'Authentication by @AuthenticationPrincipal

--- src/main/java/com/crossfit/pieds_croises/controller/ProfileController.java
+++ src/main/java/com/crossfit/pieds_croises/controller/ProfileController.java
@@ -6,7 +6,7 @@ import com.crossfit.pieds_croises.model.User;
 import com.crossfit.pieds_croises.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,22 +20,20 @@ public class ProfileController {
     }
 
     @GetMapping
-    public ResponseEntity<UserDto> getMyProfile(Authentication auth) {
-        User user = (User) auth.getPrincipal();
+    public ResponseEntity<UserDto> getMyProfile(@AuthenticationPrincipal User user) {
         UserDto userDto = userService.getMyProfile(user.getId());
         return ResponseEntity.ok(userDto);
     }
 
     @PutMapping
-    public ResponseEntity<UserDto> updateProfile(Authentication auth, @Valid @RequestBody UserUpdateDto userDetails) {
-        String username = auth.getName();
+    public ResponseEntity<UserDto> updateProfile(@AuthenticationPrincipal User user, @Valid @RequestBody UserUpdateDto userDetails) {
+        String username = user.getEmail();
         UserDto userDto = userService.updateProfile(username, userDetails);
         return ResponseEntity.ok(userDto);
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> deleteUser(Authentication auth) {
-        User user = (User) auth.getPrincipal();
+    public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal User user) {
         userService.deleteUser(user.getId());
         return ResponseEntity.noContent().build();
     }
