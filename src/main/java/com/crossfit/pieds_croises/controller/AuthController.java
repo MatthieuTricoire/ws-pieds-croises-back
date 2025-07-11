32f452d0cb0a7132f02f1ae5d31975caeff98d0e
Agattepaille <amelie.gattepaille@gmail.com>
2025-07-11 17:52:26 +0200

    feature : forgot password and reset password

--- src/main/java/com/crossfit/pieds_croises/controller/AuthController.java
+++ src/main/java/com/crossfit/pieds_croises/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.crossfit.pieds_croises.controller;
 
 import com.crossfit.pieds_croises.dto.FirstLoginDto;
+import com.crossfit.pieds_croises.dto.ForgotPasswordRequestDto;
 import com.crossfit.pieds_croises.dto.LoginRequestDto;
+import com.crossfit.pieds_croises.dto.ResetPasswordRequestDto;
 import com.crossfit.pieds_croises.security.AuthenticationService;
 import com.crossfit.pieds_croises.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,5 +34,17 @@ public class AuthController {
     public ResponseEntity<?> completeFirstLogin(@RequestBody FirstLoginDto dto) {
         userService.completeFirstLogin(dto);
         return ResponseEntity.ok("Mot de passe défini. Vous pouvez maintenant vous connecter.");
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> forgotPassword(@Valid @RequestBody ForgotPasswordRequestDto dto) {
+        authenticationService.handleForgotPassword(dto.getEmail());
+        return ResponseEntity.ok("Si cet email existe, un lien de réinitialisation a été envoyé.");
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequestDto request) {
+        authenticationService.resetPassword(request.getToken(), request.getNewPassword());
+        return ResponseEntity.ok("Mot de passe réinitialisé avec succès.");
     }
 }
