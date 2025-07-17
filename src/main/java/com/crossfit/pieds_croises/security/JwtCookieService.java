45f2b5ab329303489c87601ca26a5fa8fbc8f8b3
matthieu <matthieutricoire@gmail.com>
2025-07-17 19:30:30 +0200

    Mise en place d'un cookie sécurisé pour la connexion

--- src/main/java/com/crossfit/pieds_croises/security/JwtCookieService.java
+++ src/main/java/com/crossfit/pieds_croises/security/JwtCookieService.java
@@ -1,4 +1,25 @@
 package com.crossfit.pieds_croises.security;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class JwtCookieService {
+
+    private final JwtService jwtService;
+
+    public void addJwtCookie(HttpServletResponse response, String token) {
+        String cookieValue = "token=" + token +
+                "; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=" + (7 * 24 * 60 * 60);
+
+        response.addHeader("Set-Cookie", cookieValue);
+    }
+
+    public void removeJwtCookie(HttpServletResponse response) {
+        String cookieValue = "token=; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=0";
+        response.addHeader("Set-Cookie", cookieValue);
+    }
 }
+
