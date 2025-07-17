126a2b42e06595e06226851caa360f5ef5983b68
matthieu <matthieutricoire@gmail.com>
2025-07-17 19:30:30 +0200

    Mise en place d'un cookie sécurisé pour la connexion

--- src/main/java/com/crossfit/pieds_croises/security/JwtAuthenticationFilter.java
+++ src/main/java/com/crossfit/pieds_croises/security/JwtAuthenticationFilter.java
@@ -12,38 +12,47 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Date;
+import java.util.Arrays;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+  private final JwtService jwtService;
+  private final UserDetailsService userDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-    }
+  public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+    this.jwtService = jwtService;
+    this.userDetailsService = userDetailsService;
+  }
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String jwt = authHeader.substring(7);
-            String username = jwtService.extractClaims(jwt).getSubject();
+    String jwt = extractJwtFromCookie(request);
 
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                if (jwtService.extractClaims(jwt).getExpiration().after(new Date())) {
-                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities()
-                    );
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
-            }
+    if (org.springframework.util.StringUtils.hasText(jwt)) {
+      String username = jwtService.extractClaims(jwt).getSubject();
+
+      if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        if (jwtService.validateJwtToken(jwt)) {
+          UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+              userDetails, null, userDetails.getAuthorities());
+          SecurityContextHolder.getContext().setAuthentication(authentication);
         }
-
-        filterChain.doFilter(request, response);
+      }
     }
+    filterChain.doFilter(request, response);
+  }
+
+  private String extractJwtFromCookie(HttpServletRequest request) {
+    return request.getCookies() != null ? Arrays.stream(request.getCookies())
+        .filter(cookie -> "jwt".equals(cookie.getName()))
+        .map(cookie -> cookie.getValue())
+        .findFirst()
+        .orElse(null)
+        : null;
+  }
 }
