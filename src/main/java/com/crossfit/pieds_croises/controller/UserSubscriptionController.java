ee7514430665a16c829f87eec6206cb63b10fdd4
AGATTEPAILLE <amelie.gattepaille@gmail.com>
2025-07-22 22:36:53 +0200

    changes on UserSubscriptionController and UserSubscriptionDto, forbidden exception added again

--- src/main/java/com/crossfit/pieds_croises/controller/UserSubscriptionController.java
+++ src/main/java/com/crossfit/pieds_croises/controller/UserSubscriptionController.java
@@ -5,6 +5,8 @@ import com.crossfit.pieds_croises.service.UserSubscriptionService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -18,6 +20,7 @@ public class UserSubscriptionController {
 
     private final UserSubscriptionService userSubscriptionService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping()
     public ResponseEntity<UserSubscriptionDto> createUserSubscription(
             @Valid @RequestBody UserSubscriptionDto userSubscriptionDto) {
@@ -25,19 +28,21 @@ public class UserSubscriptionController {
         return ResponseEntity.ok(createdUserSubscription);
     }
 
-
+    @PreAuthorize("hasRole('ADMIN') or #userId == authentication.principal.id")
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<UserSubscriptionDto>> getUserSubscription(@PathVariable Long userId) {
         List<UserSubscriptionDto> userSubscriptionDto = userSubscriptionService.getAllUserSubscriptionsByUserId(userId);
         return ResponseEntity.ok(userSubscriptionDto);
     }
 
+    @PostAuthorize("hasRole('ADMIN') or returnObject.body.userId == authentication.principal.id")
     @GetMapping("/{userSubscriptionId}")
     public ResponseEntity<UserSubscriptionDto> getUserSubscriptionById(@PathVariable Long userSubscriptionId) {
         UserSubscriptionDto userSubscriptionDto = userSubscriptionService.getUserSubscriptionById(userSubscriptionId);
         return ResponseEntity.ok(userSubscriptionDto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{userSubscriptionId}/freeze")
     public ResponseEntity<Void> freezeUserSubscription(@PathVariable Long userSubscriptionId, @RequestBody Map<String, LocalDateTime> freezeDates) {
         LocalDateTime freezeStartDate = freezeDates.get("freezeStartDate");
@@ -46,6 +51,7 @@ public class UserSubscriptionController {
         return ResponseEntity.ok().build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{userSubscriptionId}")
     public ResponseEntity<Void> deleteUserSubscription(@PathVariable Long userSubscriptionId) {
         userSubscriptionService.deleteUserSubscription(userSubscriptionId);
