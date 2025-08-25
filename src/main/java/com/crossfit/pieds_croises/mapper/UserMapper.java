aad9c34f0d2e566d956e3923d3b928aeea220131
matthieu <matthieutricoire@gmail.com>
2025-08-25 12:23:28 +0200

    Add subscription id et user id to usersubscription id

--- src/main/java/com/crossfit/pieds_croises/mapper/UserMapper.java
+++ src/main/java/com/crossfit/pieds_croises/mapper/UserMapper.java
@@ -9,7 +9,10 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+    componentModel = "spring",
+    uses = { UserSubscriptionMapper.class, CourseMapper.class }
+)
 public interface UserMapper {
 
     @Mapping(target = "id", ignore = true)
