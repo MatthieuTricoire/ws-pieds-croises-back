183edfaea6d607001ca39f6a4820c8560171e389
Baptiste1828 <baptiste.delefosse@hotmail.fr>
2025-10-02 20:03:28 +0200

    Rename ExerciceController into ExerciseController

--- src/main/java/com/crossfit/pieds_croises/controller/ExerciseController.java
+++ src/main/java/com/crossfit/pieds_croises/controller/ExerciseController.java
@@ -3,6 +3,7 @@ package com.crossfit.pieds_croises.controller;
 import com.crossfit.pieds_croises.dto.ExerciceDTO;
 import com.crossfit.pieds_croises.model.Exercice;
 import com.crossfit.pieds_croises.service.ExerciseService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/exercises")
 @RequiredArgsConstructor
+@Tag(name = "Exercice", description = "Gestion des exercices")
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
@@ -34,13 +36,13 @@ public class ExerciseController {
     }
 
     @PostMapping
-    public ResponseEntity<ExerciceDTO> createExercise(@RequestBody Exercice exercise) {
+    public ResponseEntity<ExerciceDTO> createExercise(@Valid @RequestBody Exercice exercise) {
         ExerciceDTO savedExercise = exerciseService.createExercise(exercise);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedExercise);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ExerciceDTO> updateExercise(@PathVariable Long id, @RequestBody Exercice exerciseDetails) {
+    public ResponseEntity<ExerciceDTO> updateExercise(@PathVariable Long id, @Valid @RequestBody Exercice exerciseDetails) {
         ExerciceDTO updatedExercise = exerciseService.updateExercise(id, exerciseDetails);
         return ResponseEntity.ok(updatedExercise);
     }
