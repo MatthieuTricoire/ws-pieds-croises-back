2d62f3b1fd12c4ded457649f2bed7f82e13e7ca5
Agattepaille <amelie.gattepaille@gmail.com>
2025-10-12 22:26:39 +0200

    Remove data-sql auto create

--- src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
+++ src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
@@ -1,16 +1,9 @@
 package com.crossfit.pieds_croises;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
@@ -24,21 +17,9 @@ public class PiedsCroisesApplication {
         SpringApplication app = new SpringApplication(PiedsCroisesApplication.class);
         app.setAdditionalProfiles("dev");
         if (System.getProperty("spring.profiles.active") == null &&
-            System.getenv("SPRING_PROFILES_ACTIVE") == null) {
+                System.getenv("SPRING_PROFILES_ACTIVE") == null) {
             app.setAdditionalProfiles("dev");
         }
         app.run(args);
     }
-
-    @Bean
-    @Profile("!test")
-    CommandLineRunner runDataSql(DataSource dataSource) {
-        return args -> {
-            ResourceDatabasePopulator resourceDatabasePopulator =
-                new ResourceDatabasePopulator(false, false, "UTF-8", new
-                    ClassPathResource("data.sql"));
-            resourceDatabasePopulator.execute(dataSource);
-        };
-    }
-
 }
