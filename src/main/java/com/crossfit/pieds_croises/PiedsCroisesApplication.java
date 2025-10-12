9b9a4464a6786d05b3ba9afbbeed2897d436211c
matthieu <matthieutricoire@gmail.com>
2025-10-12 20:25:06 +0200

    add preprod config

--- src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
+++ src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
@@ -1,9 +1,16 @@
 package com.crossfit.pieds_croises;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
@@ -23,15 +30,15 @@ public class PiedsCroisesApplication {
         app.run(args);
     }
 
-    // @Bean
-    // @Profile("!test")
-    // CommandLineRunner runDataSql(DataSource dataSource) {
-    // return args -> {
-    // ResourceDatabasePopulator resourceDatabasePopulator =
-    // new ResourceDatabasePopulator(false, false, "UTF-8", new
-    // ClassPathResource("dont-use-db.data.sql"));
-    // resourceDatabasePopulator.execute(dataSource);
-    // };
-    // }
+    @Bean
+    @Profile("!test")
+    CommandLineRunner runDataSql(DataSource dataSource) {
+        return args -> {
+            ResourceDatabasePopulator resourceDatabasePopulator =
+                new ResourceDatabasePopulator(false, false, "UTF-8", new
+                    ClassPathResource("data.sql"));
+            resourceDatabasePopulator.execute(dataSource);
+        };
+    }
 
 }
