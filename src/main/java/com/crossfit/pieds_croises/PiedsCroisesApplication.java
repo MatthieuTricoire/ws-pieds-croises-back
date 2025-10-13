1ef8f1a5bd6dfb971c9851d847501213afa313f4
Agattepaille <amelie.gattepaille@gmail.com>
2025-10-13 22:17:25 +0200

    change launch profile exception for tests

--- src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
+++ src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
@@ -31,7 +31,7 @@ public class PiedsCroisesApplication {
     }
 
     @Bean
-    @Profile("!prod")
+    @Profile("!test")
     CommandLineRunner runDataSql(DataSource dataSource) {
         return args -> {
             ResourceDatabasePopulator resourceDatabasePopulator =
