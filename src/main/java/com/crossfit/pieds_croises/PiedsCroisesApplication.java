188f949a5a05cfe41cafa24b2404d46da951118a
Agattepaille <amelie.gattepaille@gmail.com>
2025-10-13 19:33:09 +0200

    add data.sql, change launch profile

--- src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
+++ src/main/java/com/crossfit/pieds_croises/PiedsCroisesApplication.java
@@ -1,9 +1,16 @@
 package com.crossfit.pieds_croises;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
@@ -21,5 +28,15 @@ public class PiedsCroisesApplication {
             app.setAdditionalProfiles("dev");
         }
         app.run(args);
+    }
+
+    @Bean
+    @Profile("preprod | dev")
+    CommandLineRunner runDataSql(DataSource dataSource) {
+        return args -> {
+            ResourceDatabasePopulator resourceDatabasePopulator =
+                    new ResourceDatabasePopulator(false, false, "UTF-8", new ClassPathResource("data.sql"));
+            resourceDatabasePopulator.execute(dataSource);
+        };
     }
 }
